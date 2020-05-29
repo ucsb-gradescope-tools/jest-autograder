6c28616cdd0a36929a717aeffda459cd7fe6031b
Cole Bergmann <43765292+colebergmann@users.noreply.github.com>
2020-05-29 00:43:27 -0700

    cb - alpha release of autograder

--- tools/parse_mutations_csv.py
+++ tools/parse_mutations_csv.py
@@ -1,13 +1,32 @@
-import xml.etree.ElementTree as ET
 import argparse
 import os
+import csv
 import json
 
 # Initialize command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--reportpath", help="Directory for pit-reports")
+parser.add_argument("--max_points", help="Maximum points for the assignment, divided among the test cases")
+parser.add_argument("-o", "--output", help="Json file to output")
 args = parser.parse_args()
 
-if args.reportpath != None:
+if args.reportpath != None and args.max_points != None and args.output != None:
+    killed = 0
+    total = 0
     specific_folder = [x[0] for x in os.walk(args.reportpath)][1]
-    os.chdir(specific_folder)
+    with open(os.path.join(specific_folder, "mutations.csv"), newline='') as csvfile:
+        f = csv.reader(csvfile, delimiter=',')
+        for r in f:
+            total += 1
+            if r[5] == "SURVIVED":
+                killed += 1
+
+    score = (killed/total) * float(args.max_points)
+    the_test = {
+        "name": "Run student test suite against multiple mutant codebases", 
+        "max_score": float(args.max_points), 
+        "score": score,
+        "output":"Correctly identified " + str(killed) + " mutants of " + str(total) + " total mutants"
+        }
+    with open(args.output, 'w') as outfile:
+        json.dump({ "tests" : [ the_test ] }, outfile)
