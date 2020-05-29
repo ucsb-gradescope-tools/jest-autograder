1d93ad06657e14be5120097b5a616f2de5b43fcd
Cole Bergmann <43765292+colebergmann@users.noreply.github.com>
2020-05-28 23:42:45 -0700

    cb - branched

--- localautograder/submission/src/test/java/edu/ucsb/gradescope/example/AnimalConstructorTest.java
+++ localautograder/submission/src/test/java/edu/ucsb/gradescope/example/AnimalConstructorTest.java
@@ -1,9 +1,13 @@
 package edu.ucsb.gradescope.example;
 
+import autograder.GradescopeTestClass;
+import com.github.tkutcher.jgrade.gradedtest.GradedTest;
 import org.junit.Test;
 
+@GradescopeTestClass
 public class AnimalConstructorTest {
     @Test
+    @GradedTest(name="Default constructor - new Animal()", points=10)
     public void testDefaultConstructor() {
         Animal animal = new Animal();
         assert(animal.getName().equals("Ellie"));
@@ -12,6 +16,7 @@ public class AnimalConstructorTest {
     }
 
     @Test
+    @GradedTest(name="Constructor with arguments - new Animal(\"Elewardo\", 5, 6)", points=10)
     public void testConstructorWithParams() {
         Animal animal = new Animal("Elewardo", 5, 6);
         assert(animal.getName().equals("Elewardo"));
@@ -20,6 +25,7 @@ public class AnimalConstructorTest {
     }
 
     @Test
+    @GradedTest(name="add(1,2)=3", points=10)
     public void testAdd() {
         assert(Animal.add(1, 2) == 3);
     }
