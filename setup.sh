83b8444f6bb9d97c17d2b95562d9a6cf6c95933b
Scott Chow <tehchowster@gmail.com>
2020-09-29 11:45:09 -0700

    sc - update setup.sh to install correct version of nodejs and npm

--- setup.sh
+++ setup.sh
@@ -3,11 +3,15 @@
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
 apt install -y openjdk-11-jdk
-apt install -y node
 
 apt-get -y install rsync
 apt-get -y install maven
 apt-get -y install bash
+
+curl -sL https://deb.nodesource.com/setup_lts.x | bash -
+apt install -y nodejs
+apt install -y npm
+npm install npm@latest -g
 
 # The following lines are to make sure we can install packages from maven
 # See https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/1396760
